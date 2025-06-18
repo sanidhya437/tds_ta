badddaabc4f53fefa5e9f149001fa51a28e000de
sanidhya437 <77682286+sanidhya437@users.noreply.github.com>
2025-06-18 10:31:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
-
+import os
 app = Flask(__name__)
+CORS(app)
+
 
    # Load scraped data
 with open('discourse_posts.json') as f:
