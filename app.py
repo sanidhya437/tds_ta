caa4741ae2ae75c0768968289fa1bdfa70dfe4e6
sanidhya437 <77682286+sanidhya437@users.noreply.github.com>
2025-06-18 23:11:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -42,6 +43,7 @@ def answer_question():
        }
        return jsonify(response)
 
+
 if __name__ == '__main__':
-       app.run()
-   
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
